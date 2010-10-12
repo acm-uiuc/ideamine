08edd54c97f6491fb04061e8f2e3e8907d8b7899
Dylan Nugent <dnugent@orion.(none)>
2010-10-12 14:35:25 -0500

    Added rudimentary models.py file framework.

--- ideamine/main/models.py
+++ ideamine/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Idea(models.Model):
+    owner = models.ForeignKey(User)
+    shortName = models.CharField(max_length=50)
+    desc = models.CharField(max_length=500)
+    tags = models.ManyToManyField(Tag)
+    members = models.ManyToManyField(User)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+
+# Working on a better way to implement this
+class Comment(models.Model):
+    owner = models.ForeignKey(User)
+    location = models.ForeignKey(Idea)
+    parent = models.ForeignKey(Comment)
+    text = models.CharField(max_length=300)
