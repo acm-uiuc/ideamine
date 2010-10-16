62f0bed62ab5bd99bf53654eda77d971083ee1b7
Dylan Nugent <nugent5@illinois.edu>
2010-10-16 11:54:26 -0500

    Enabled admin interface

--- ideamine/settings.py
+++ ideamine/settings.py
@@ -89,8 +89,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
--- ideamine/urls.py
+++ ideamine/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'main.views.index'),
     # Example:
     # (r'^ideamine/', include('ideamine.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
