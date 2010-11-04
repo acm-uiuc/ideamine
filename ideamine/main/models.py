15e8c86add442bff6705875c082b3113ba5bf3ae
Dylan Nugent <nugent5@illinois.edu>
2010-11-04 14:14:04 -0500

    Added unicode return strings, working on commend model implementation.

--- ideamine/main/models.py
+++ ideamine/main/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Tag(models.Model):
     name = models.CharField(max_length=25)
 
+    def __unicode__(self):
+        return self.name
+
 class Idea(models.Model):
     owner = models.ForeignKey(User, related_name='+')
     shortName = models.CharField(max_length=50)
@@ -11,8 +14,12 @@ class Idea(models.Model):
     tags = models.ManyToManyField(Tag)
     # So this line doesn't work. I don't know why (yet)
     #members = models.ManyToManyField(User, related_name='+')
+    
+    def __unicode__(self):
+        return self.shortName + ': ' + self.owner
 
 # Working on a better way to implement this
+# Note ambiguity is present in the fields; working on this (hence no unicode call yet)
 class Comment(models.Model):
     owner = models.ForeignKey(User, related_name='+')
     location = models.ForeignKey(Idea)
