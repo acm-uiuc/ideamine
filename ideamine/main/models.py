24169dd97a6ebac6949cc07ca36e7e4eb2979fe7
Dylan Nugent <nugent5@illinois.edu>
2010-11-04 11:40:47 -0500

    Update to models, still semi-broken.

--- ideamine/main/models.py
+++ ideamine/main/models.py
@@ -5,15 +5,16 @@ class Tag(models.Model):
     name = models.CharField(max_length=25)
 
 class Idea(models.Model):
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, related_name='+')
     shortName = models.CharField(max_length=50)
     desc = models.CharField(max_length=500)
     tags = models.ManyToManyField(Tag)
-    members = models.ManyToManyField(User)
+    # So this line doesn't work. I don't know why (yet)
+    #members = models.ManyToManyField(User, related_name='+')
 
 # Working on a better way to implement this
 class Comment(models.Model):
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, related_name='+')
     location = models.ForeignKey(Idea)
     parent = models.ForeignKey('self')
     text = models.CharField(max_length=300)
