e8c7947af07e1299922fa4ec836db6a7fd6c4e5c
Dylan Nugent <nugent5@illinois.edu>
2010-11-04 10:43:22 -0500

    Fully working models implementation. We need to find a better way to do absoloute path settings in git.

--- ideamine/main/models.py
+++ ideamine/main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+
 class Idea(models.Model):
     owner = models.ForeignKey(User)
     shortName = models.CharField(max_length=50)
@@ -8,12 +11,9 @@ class Idea(models.Model):
     tags = models.ManyToManyField(Tag)
     members = models.ManyToManyField(User)
 
-class Tag(models.Model):
-    name = models.CharField(max_length=25)
-
 # Working on a better way to implement this
 class Comment(models.Model):
     owner = models.ForeignKey(User)
     location = models.ForeignKey(Idea)
-    parent = models.ForeignKey(Comment)
+    parent = models.ForeignKey('self')
     text = models.CharField(max_length=300)
--- ideamine/settings.py
+++ ideamine/settings.py
@@ -81,7 +81,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/sean/Python/ideamine/ideamine/templates"
+    "/home/sean/Python/ideamine/ideamine/templates",
+    "/home/dnugent/Documents/Programming/projects/ideamine/ideamine/templates"
 )
 
 INSTALLED_APPS = (
