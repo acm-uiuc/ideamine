3d564555f3ecbec12d6f29df6725f7dbc214abc3
Sean Nicolay <nicolay2@illinois.edu>
2010-11-22 17:37:09 -0600

    wrote views/templates for individual objects
    Once again this is using django's built in generic views (they're handy!). Also changed the list views to display ids as manual urls are the only way to get to object pages since I haven't written any sort of links into any of the templates, yet.

--- ideamine/main/views.py
+++ ideamine/main/views.py
@@ -1,16 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from models import Idea
+from models import Idea, User
 
 def index(request):
     return render_to_response("main.html",
                               { "h1" : "Index"})
-
-def user(request, user_id):
-    return render_to_response("user.html",
-                              { "user" : User.objects.filter(id=user_id) })
-
-def idea(request, request_id):
-    return render_to_response("idea.html",
-                              { "idea" : Idea.objects.filter(id=request_id) })
--- ideamine/urls.py
+++ ideamine/urls.py
@@ -5,14 +5,21 @@ from main.models import Idea, User
 
 admin.autodiscover()
 
+users = { 'queryset' : User.objects.all() }
+ideas = { 'queryset' : Idea.objects.all() }
+
 urlpatterns = patterns('',
     (r'^$', 'main.views.index'),
-    (r'^users/$', list_detail.object_list, {"queryset" : User.objects.all(), "template_name" : "users.html"}),
-    (r'^users/(\d+)/$', 'main.views.user'),
-    (r'^ideas/$', list_detail.object_list, {"queryset" : Idea.objects.all(), "template_name" : "ideas.html"}),
-    (r'^ideas/(\d+)/$', 'main.views.idea'),
-    # Example:
-    # (r'^ideamine/', include('ideamine.foo.urls')),
+
+    (r'^users/$', list_detail.object_list,
+     dict(users, template_name='users.html')),
+    (r'^users/(?P<object_id>\d+)/$', list_detail.object_detail,
+     dict(users, template_name='user.html', template_object_name='user')),
+
+    (r'^ideas/$', list_detail.object_list,
+     dict(ideas, template_name="ideas.html")),
+    (r'^ideas/(?P<object_id>\d+)/$', list_detail.object_detail,
+     dict(ideas, template_name='idea.html', template_object_name='idea')),
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
