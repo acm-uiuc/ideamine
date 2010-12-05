2545a03d5f7f1015a2e40bb60e172f3279e633e9
Sean Nicolay <nicolay2@illinois.edu>
2010-12-04 23:27:15 -0600

    Renamed Ideas#modified_at to Ideas#updated_at
    Also removed my timestamp updating on save code. There are attributes for these built into Django::DB::Models::DateTimeField.

--- ideamine/main/models.py
+++ ideamine/main/models.py
@@ -13,15 +13,9 @@ class Idea(models.Model):
     short_name = models.CharField(max_length=50)
     desc = models.TextField()
     tags = models.ManyToManyField(Tag)
-    created_at = models.DateTimeField(blank=True)
-    modified_at = models.DateTimeField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     members = models.ManyToManyField(User, related_name='joined_ideas')
     
     def __unicode__(self):
         return self.short_name + ': ' + self.owner.username
-
-    def save(self):
-        if self.created_at == None:
-            self.created_at = datetime.now()
-        self.modified_at = datetime.now()
-        super(Idea, self).save()
