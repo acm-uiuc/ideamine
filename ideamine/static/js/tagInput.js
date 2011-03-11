86ea8eca12a9482fefd5056761e3046ae693a638
Taylor Fairbank <jtfairbank@gmail.com>
2011-03-11 04:16:53 -0600

    Style Updates and Textbox List Code for Testing
    The header has been mostly styled (except for logo).  This sets a
    precedent for a minimalistic look/feel with a redscale / grayscale color
    scheme.  I'm also committing the Textbox List code to determine what
    happens on for submit to the server.

--- ideamine/static/js/tagInput.js
+++ ideamine/static/js/tagInput.js
@@ -1,21 +1,25 @@
 $(function() { // onload
 	// sets up a textbox list for inputing tags
-	// requires an input element with id='id_tags'
-	var t = new $.TextboxList('input#id_tags', {
+	// requires an input element with id='id_tags_field'
+	var t = new $.TextboxList('input#id_tags_field', {
 	    bitsOptions: {editable: {addKeys: [32]}}, // 32 = space key code
 	    unique: true,
 	    plugins: {autocomplete: {
 	    	minLength: 2,
 	    	queryRemote: true,
-	    	remote: {url: 'URL_GOES_HERE.php'} // REPLACE URL WITH A VALID ONE
+	    	remote: {url: '/tags/suggest'}
 	    }}
 	});
 	
 	// expects existing input to be placed in the input box (space delimited)
-	if ($("input#id_tags").text()) {
-		var existingTags = $("input#id_tags").text().split(' ');
+	if ($("input#id_tags_field").text()) {
+		var existingTags = $("input#id_tags_field").text().split(' ');
 		for (var i = 0; i < existingTags.length; i++) {
 		    t.add(existingTags[i]);
 		}
 	}
+	
+	$("button#ideaSubmit").click(function() {
+		
+	});
 });
